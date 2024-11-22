dfe90991e8a32b87aaf0f46c41491f0fcdc9ffc1
Krishna Mohan Koyya <krishna@glarimy.com>
2024-11-23 01:51:12 +0530

    added logging

--- src/main/java/com/glarimy/echo/EchoServer.java
+++ src/main/java/com/glarimy/echo/EchoServer.java
@@ -1,13 +1,20 @@
 package com.glarimy.echo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EchoServer {
+    private static final Logger logger = LoggerFactory.getLogger(EchoServer.class);
+
     @GetMapping("/echo/{name}")
-    public String echo(@PathVariable("name") String name){
-        return "Hello, " + name + "! Welcome to DevOps.\n";
+    public String echo(@PathVariable("name") String name) {
+        logger.debug("Received echo request with name " + name);
+        String response = "Hello, " + name + "! Welcome to DevOps.\n";
+        logger.debug("Returning response " + response);
+        return response;
     }
 }
